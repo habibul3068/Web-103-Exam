7fff51c1b399d89751ed5e2ddea9352e976dd0ab
habibul3068 <habibulgoni3068@gmail.com>
2025-11-27 13:26:28 +0600

    Undo

--- js/index.js
+++ js/index.js
@@ -4,32 +4,28 @@ var weatherInfo = [
     { district: "Khulna", temperature: 30, condition: "Cloudy" }
 ];
 
+// --- PART B: The Function ---
 function checkWeather(){
     var selectBox  = document.getElementById("select");
     var userChoice = selectBox.value;
 
     var imageTag = document.getElementById("image");
     var errorTag = document.getElementById("error");
-    var statusTag = document.getElementById("status"); // NEW TEXT
 
     // Reset display
     imageTag.style.display = "none";
-    statusTag.innerHTML = "";
     errorTag.innerHTML = "";
 
     if(userChoice === ""){
         errorTag.innerHTML = "Warning: Please select a city!";
     } 
-    else {
+    else(userChoice === "")
+    {
         var result = weatherInfo.find(item => item.district === userChoice);
 
         if(result){
-            // Set image
             imageTag.src = "../img/" + result.condition + ".png"; 
             imageTag.style.display = "inline-block";
-
-            // Set weather text
-            statusTag.innerHTML = "Weather Status: " + result.condition;
         }
     }
 }
